637561e4a3830e8bf1c49861e8ea008f1107d4dc
momtinkao <momtinkao@gamil.com>
2024-09-25 12:25:21 +0800

    new radar.py

--- radar.py
+++ radar.py
@@ -1,4 +1,4 @@
-#python3.8.0 64位（python 32位要用32位的DLL/so(Linux)）
+# python3.8.0 64位（python 32位要用32位的DLL/so(Linux)）
 #
 from ctypes import *
 import ctypes
@@ -8,7 +8,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
-
+import datetime
+import socket
+import struct
+path = 'obj5.log'
+f = open(path, 'w')
 logging.basicConfig(level=logging.DEBUG,
                     filename='objlist.log',
                     filemode='a',
@@ -34,6 +38,14 @@ class VCI_CAN_OBJ(Structure):
 
 byte_array = c_ubyte * 8
 texts = []
+type_name = ["point", "car", "truck", "pedestrian",
+             "motor", "bicycle", "wide", "other"]
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+message = b"hello,Server"
+server_address = ('172.30.114.34', 12346)
+client_address = ('172.30.112.1', 10006)
+client_socket.bind(client_address)
+client_socket.sendto(message, server_address)
 
 
 class Radar_Config:
@@ -147,7 +159,7 @@ class Radar_Config:
         self.buf[4] |= (c_ubyte(val).value & 0x07) << 0
 
 
-#Radar_state class
+# Radar_state class
 class Radar_State:
 
     def __init__(self):
@@ -203,10 +215,16 @@ class FilterCfg:
             val = (int)(val // 0.1)
         if self.index == 0x7:
             val = (int)(val // 0.025)
-        if self.index == 0xb:
+        if self.index == 0xc:
             val = (int)(val // 0.0315)
-        if self.index == 0xd:
+        if self.index == 0xe:
             val = (int)(val // 0.0315)
+        if self.index == 0x3:
+            val = (int)(val // 0.0315)
+        if self.index == 0x4:
+            val = (int)(val // 0.0315)
+        if self.index == 0x5:
+            val = (int)((val + 50) // 0.025)
         self.buf[1] &= ~(0x0f)
         self.buf[1] |= (c_ubyte(c_ushort(val).value >> 8).value & 0x0f)
         self.buf[2] &= ~(0xff)
@@ -217,10 +235,16 @@ class FilterCfg:
             val = (int)(val // 0.025)
         if self.index == 0x1:
             val = (int)(val // 0.1)
-        if self.index == 0xb:
+        if self.index == 0xc:
             val = (int)(val // 0.0315)
-        if self.index == 0xd:
+        if self.index == 0xe:
             val = (int)(val // 0.0315)
+        if self.index == 0x3:
+            val = (int)(val // 0.0315)
+        if self.index == 0x4:
+            val = (int)(val // 0.0315)
+        if self.index == 0x5:
+            val = (int)((val + 50) // 0.025)
         self.buf[3] &= ~(0x0f)
         self.buf[3] |= (c_ubyte(c_ushort(val).value >> 8).value & 0x0f)
         self.buf[4] &= ~(0xff)
@@ -279,16 +303,26 @@ class FilterStatus:
             min_prob = (((c_ushort(self.buf[1]).value & 0x0f) << 8) |
                         (c_ubyte(self.buf[2] & 0xff).value))
             print(f"min prob:{min_prob}")
-        if self.index == 0xb:
+        if self.index == 0xc:
             min_vyrightleft = 0.0315 * ((
                 (c_ushort(self.buf[1]).value & 0x0f) << 8) |
-                                        (c_ubyte(self.buf[2] & 0xff).value))
-            print(f"min vyright2left:{min_vyrightleft}")
-        if self.index == 0xd:
+                (c_ubyte(self.buf[2] & 0xff).value))
+            print(f"min voncocme:{min_vyrightleft}")
+        if self.index == 0xe:
             min_vyleftright = 0.0315 * ((
                 (c_ushort(self.buf[1]).value & 0x0f) << 8) |
-                                        (c_ubyte(self.buf[2] & 0xff).value))
-            print(f"min vyleft2right:{min_vyleftright}")
+                (c_ubyte(self.buf[2] & 0xff).value))
+            print(f"min vdepart:{min_vyleftright}")
+        if self.index == 0x3:
+            min_come_vrel = 0.0315 * ((
+                (c_ushort(self.buf[1]).value & 0x0f) << 8) |
+                (c_ubyte(self.buf[2]).value & 0xff))
+            print("min come_vrel:", min_come_vrel)
+        if self.index == 0x5:
+            min_RCS = 0.025 * ((
+                (c_ushort(self.buf[1]).value & 0x0f) << 8) |
+                (c_ubyte(self.buf[2]).value & 0xff)) - 50
+            print("min RCS:", min_RCS)
 
     def GET_FilterCfg_FilterCfg_Max_Class(self):
         if self.index == 0x7:
@@ -299,16 +333,31 @@ class FilterStatus:
             max_prob = (((c_ushort(self.buf[3]).value & 0x0f) << 8) |
                         (c_ubyte(self.buf[4] & 0xff).value))
             print(f"min prob:{max_prob}")
-        if self.index == 0xb:
+        if self.index == 0xc:
             max_vyrightleft = 0.0315 * ((
                 (c_ushort(self.buf[3]).value & 0x0f) << 8) |
-                                        (c_ubyte(self.buf[4]).value & 0xff))
-            print("max vyright2left:", max_vyrightleft)
-        if self.index == 0xd:
+                (c_ubyte(self.buf[4]).value & 0xff))
+            print("max voncome:", max_vyrightleft)
+        if self.index == 0xe:
             max_vyleftright = 0.0315 * ((
                 (c_ushort(self.buf[3]).value & 0x0f) << 8) |
-                                        (c_ubyte(self.buf[4]).value & 0xff))
-            print("max vyleft2right:", max_vyleftright)
+                (c_ubyte(self.buf[4]).value & 0xff))
+            print("max vdepart:", max_vyleftright)
+        if self.index == 0x3:
+            max_come_vrel = 0.0315 * ((
+                (c_ushort(self.buf[3]).value & 0x0f) << 8) |
+                (c_ubyte(self.buf[4]).value & 0xff))
+            print("max come_vrel:", max_come_vrel)
+        if self.index == 0x4:
+            max_dep_vrel = 0.0315 * ((
+                (c_ushort(self.buf[3]).value & 0x0f) << 8) |
+                (c_ubyte(self.buf[4]).value & 0xff))
+            print("max dep_vrel:", max_dep_vrel)
+        if self.index == 0x5:
+            max_RCS = 0.025 * ((
+                (c_ushort(self.buf[3]).value & 0x0f) << 8) |
+                (c_ubyte(self.buf[4]).value & 0xff)) - 50
+            print("max RCS:", max_RCS)
 
     def GET_FilterCfg_FilterCfg_Index(self):
         self.index = (c_ubyte(self.buf[0]).value >> 3) & 0x0f
@@ -344,10 +393,16 @@ class Object_list:
     def print_object(self):
         self.mutex.acquire()
         global points
-        points = [(0, 0, 'a')]
+        points = [(0, 0, 'None')]
+        now = datetime.datetime.now()
+        byte = struct.pack("!IIIfI", 1, now.hour,
+                           now.minute, (float)(now.second + now.microsecond / 1000000), self.length)
         for it in self.object_list:
             if it != None:
-                points.append((it.distlat, it.distlong, str(it.id)))
+                points.append((it.distlat, it.distlong, type_name[it.type]))
+                byte = byte + \
+                    struct.pack("!ddI", it.distlat, it.distlong, it.type)
+        client_socket.sendto(byte, server_address)
         self.mutex.release()
 
 
@@ -358,7 +413,7 @@ class Object:
         self.distlong = 1000
         self.distlat = 1000
         self.vrelong = 1000
-        self.type = 10
+        self.type = None
         self.state = 6
 
     def get_obj_ID(self, buf):
@@ -370,11 +425,11 @@ class Object:
 
     def get_obj_distlat(self, buf):
         self.distlat = (((c_ushort(buf[2]).value & 0x07) << 8) |
-                        ((c_ubyte(buf[3]).value >> 0) & 0xff)) * 0.2 - 204.6
+                        ((c_ubyte(buf[3]).value >> 0) & 0xff)) * 0.2 - 209.6
 
-    def get_obj_vrelat(self, buf):
-        self.vrelong = (((c_ushort(buf[5]).value & 0x3f) << 3) |
-                        ((c_ubyte(buf[6]).value >> 5) & 0x07)) * 0.25 - 64
+    def get_obj_vrelong(self, buf):
+        self.vrelong = (((((c_ushort(buf[4]).value & 0xff) << 8) |
+                        ((c_ubyte(buf[5]).value >> 6) & 0x03)) * 0.25 - 128) * 3.6)
 
 
 def get_filterNums(buf):
@@ -387,8 +442,8 @@ scat = ax.scatter([], [], s=100)
 
 
 def init():
-    ax.set_xlim(-50, 50)
-    ax.set_ylim(0, 50)
+    ax.set_xlim(-30, 30)
+    ax.set_ylim(0, 260)
     for text in texts:
         text.remove()
     texts.clear()
@@ -415,11 +470,11 @@ def update(frame):
     return scat, *texts
 
 
-#CanDLLName = './ControlCAN.dll' #把DLL放到对应的目录下
-CanDLLName = './ControlCAN.so'  #把SO放到对应的目录下,LINUX
-#canDLL = windll.LoadLibrary('./ControlCAN.dll')
-#Linux系统下使用下面语句，编译命令：python3 python3.8.0.py
-canDLL = cdll.LoadLibrary('./libcontrolcan.so')
+# CanDLLName = './ControlCAN.dll' #把DLL放到对应的目录下
+CanDLLName = './ControlCAN.dll'  # 把SO放到对应的目录下,LINUX
+# canDLL = windll.LoadLibrary('./ControlCAN.dll')
+# Linux系统下使用下面语句，编译命令：python3 python3.8.0.py
+canDLL = windll.LoadLibrary('./ControlCan.dll')
 
 print(CanDLLName)
 
@@ -429,9 +484,9 @@ if ret == STATUS_OK:
 if ret != STATUS_OK:
     print('调用 VCI_OpenDevice出错\r\n')
 
-#初始0通道
+# 初始0通道
 vci_initconfig = VCI_INIT_CONFIG(0x80000008, 0xFFFFFFFF, 0, 0, 0x00, 0x1C,
-                                 0)  #波特率500k，正常模式
+                                 0)  # 波特率500k，正常模式
 ret = canDLL.VCI_InitCAN(VCI_USBCAN2, 0, 0, byref(vci_initconfig))
 if ret == STATUS_OK:
     print('调用 VCI_InitCAN1成功\r\n')
@@ -450,24 +505,26 @@ class VCI_CAN_OBJ_ARRAY(Structure):
                 ('STRUCT_ARRAY', ctypes.POINTER(VCI_CAN_OBJ))]
 
     def __init__(self, num_of_structs):
-        #这个括号不能少
+        # 这个括号不能少
         self.STRUCT_ARRAY = ctypes.cast((VCI_CAN_OBJ * num_of_structs)(),
-                                        ctypes.POINTER(VCI_CAN_OBJ))  #结构体数组
-        self.SIZE = num_of_structs  #结构体长度
-        self.ADDR = self.STRUCT_ARRAY[0]  #结构体数组地址  byref()转c地址
+                                        ctypes.POINTER(VCI_CAN_OBJ))  # 结构体数组
+        self.SIZE = num_of_structs  # 结构体长度
+        self.ADDR = self.STRUCT_ARRAY[0]  # 结构体数组地址  byref()转c地址
 
 
-rx_vci_can_obj = VCI_CAN_OBJ_ARRAY(2500)  #结构体数组
+rx_vci_can_obj = VCI_CAN_OBJ_ARRAY(2500)  # 结构体数组
 
 obj_list = Object_list()
 
 
 def receive():
     global prev
-    while 1:  #一直循环查询接收。
+    global lock
+    idx = 1
+    while 1:  # 一直循环查询接收。
         ret = canDLL.VCI_Receive(VCI_USBCAN2, 0, 0, byref(rx_vci_can_obj.ADDR),
                                  2500, 0)
-        if ret > 0:  #接收到数据
+        if ret > 0:  # 接收到数据
             for i in range(0, ret):
                 '''
                 if rx_vci_can_obj.STRUCT_ARRAY[i].ID == 0x201:
@@ -478,6 +535,10 @@ def receive():
                         f"radar max distance:{radar_status.RadarState_MaxDistanceCfg()}"
                     )
                 '''
+                if rx_vci_can_obj.STRUCT_ARRAY[i].ID == 0x201:
+                    byte = bytearray(rx_vci_can_obj.STRUCT_ARRAY[i].Data)
+                    f.write(
+                        f"({format(time.time(),'.6f')}) can0 201#{byte.hex()}\n")
                 if rx_vci_can_obj.STRUCT_ARRAY[i].ID == 0x60a:
                     obj_list.length = (c_ubyte(
                         rx_vci_can_obj.STRUCT_ARRAY[i].Data[0]).value)
@@ -485,6 +546,9 @@ def receive():
                         rx_vci_can_obj.STRUCT_ARRAY[i].Data[1]).value << 8) | (
                             c_ubyte(
                                 rx_vci_can_obj.STRUCT_ARRAY[i].Data[2]).value)
+                    byte = bytearray(rx_vci_can_obj.STRUCT_ARRAY[i].Data[0:4])
+                    f.write(
+                        f"({format(time.time(),'.6f')}) can0 60A#{byte.hex()}\n")
                     obj_list.print_object()
                     obj_list.clear_list()
                 if rx_vci_can_obj.STRUCT_ARRAY[i].ID == 0x60b:
@@ -494,6 +558,9 @@ def receive():
                     obj.get_obj_distlong(rx_vci_can_obj.STRUCT_ARRAY[i].Data)
                     obj.get_obj_vrelong(rx_vci_can_obj.STRUCT_ARRAY[i].Data)
                     obj_list.insert_object(obj)
+                    byte = bytearray(rx_vci_can_obj.STRUCT_ARRAY[i].Data)
+                    f.write(
+                        f"({format(time.time(),'.6f')}) can0 60B#{byte.hex()}\n")
                 if rx_vci_can_obj.STRUCT_ARRAY[i].ID == 0x60c:
                     obj_id = obj_id = c_ubyte(
                         rx_vci_can_obj.STRUCT_ARRAY[i].Data[0]).value
@@ -502,69 +569,83 @@ def receive():
                         >> 2) & 0x07
                     if obj_list.object_list[obj_id] != None:
                         obj_list.object_list[obj_id].state = state
+                    byte = bytearray(rx_vci_can_obj.STRUCT_ARRAY[i].Data[0:7])
+                    f.write(
+                        f"({format(time.time(),'.6f')}) can0 60C#{byte.hex()}\n")
                 if rx_vci_can_obj.STRUCT_ARRAY[i].ID == 0x60D:
                     obj_id = c_ubyte(
                         rx_vci_can_obj.STRUCT_ARRAY[i].Data[0]).value
                     if obj_list.object_list[obj_id] != None:
                         obj_list.object_list[obj_id].type = (c_ubyte(
                             rx_vci_can_obj.STRUCT_ARRAY[i].Data[3]).value
-                                                             & 0x07)
-                        points.append((obj_list.object_list[obj_id].distlat,
-                                       obj_list.object_list[obj_id].distlong,
-                                       str(obj_list.object_list[obj_id].id)))
+                            & 0x07)
+                    byte = bytearray(rx_vci_can_obj.STRUCT_ARRAY[i].Data)
+                    f.write(
+                        f"({format(time.time(),'.6f')}) can0 60D#{byte.hex()}\n")
                 if rx_vci_can_obj.STRUCT_ARRAY[i].ID == 0x203:
                     filter_nums = get_filterNums(
                         rx_vci_can_obj.STRUCT_ARRAY[i].Data)
                     print("filter_nums:", filter_nums)
-                if (rx_vci_can_obj.STRUCT_ARRAY[i].ID == 0x204):
+                if (rx_vci_can_obj.STRUCT_ARRAY[i].ID == 0x204 and idx <= 13):
                     filter_status.buffer_filling(
                         rx_vci_can_obj.STRUCT_ARRAY[i].Data)
                     filter_status.GET_FilterCfg_FilterCfg_Index()
                     filter_status.GET_FilterCfg_FilterCfg_Min_Class()
                     filter_status.GET_FilterCfg_FilterCfg_Max_Class()
                     filter_status.Get_FilterCfg_FilterCfg_Active()
-                    if filter_status.index < 0xe:
-                        filter_config = FilterCfg()
-                        filter_config.FilterCfg_FilterCfg_Index(
-                            filter_status.index + 1)
-                        filter_config.FilterCfg_FilterCfg_Type(1)
-                        filter_config.FilterCfg_FilterCfg_Min_Class(0)
-                        filter_config.FilterCfg_FilterCfg_Max_Class(0)
-                        filter_config.FilterCfg_FilterCfg_Valid(0)
-                        filter_config.FilterCfg_FilterCfg_Active(0)
-                        filter_object = VCI_CAN_OBJ(0x202, 0, 0, 1, 0, 0, 8,
-                                                    filter_config.buf, b)
-                        canDLL.VCI_Transmit(VCI_USBCAN2, 0, 0,
-                                            byref(filter_object), 1)
+                    filter_config = FilterCfg()
+                    filter_config.FilterCfg_FilterCfg_Index(idx)
+                    idx += 1
+                    filter_config.FilterCfg_FilterCfg_Type(1)
+                    filter_config.FilterCfg_FilterCfg_Min_Class(0)
+                    filter_config.FilterCfg_FilterCfg_Max_Class(0)
+                    filter_config.FilterCfg_FilterCfg_Valid(0)
+                    filter_config.FilterCfg_FilterCfg_Active(0)
+                    filter_object = VCI_CAN_OBJ(
+                        0x202, 0, 0, 1, 0, 0, 8, filter_config.buf, b)
+
+                    t = threading.Thread(target=transmit,args=(filter_object,))
+                    t.start()
+                    t.join()
+
+
+lock = threading.Lock()
+
+
+def transmit(msg):
+    global lockl
+    lock.acquire()
+    ret1 = canDLL.VCI_Transmit(VCI_USBCAN2, 0, 0, byref(msg), 1)
+    lock.release()
 
 
 r = threading.Thread(target=receive)
 r.start()
 
-#size Filter
+# size Filter
 filter_status = FilterStatus()
 filter_config_size = FilterCfg()
-filter_config_size.FilterCfg_FilterCfg_Index(0x7)
+filter_config_size.FilterCfg_FilterCfg_Index(0xc)
 filter_config_size.FilterCfg_FilterCfg_Type(1)
-filter_config_size.FilterCfg_FilterCfg_Min_Class(0)
-filter_config_size.FilterCfg_FilterCfg_Max_Class(100)
+filter_config_size.FilterCfg_FilterCfg_Min_Class(3)
+filter_config_size.FilterCfg_FilterCfg_Max_Class(30)
 filter_config_size.FilterCfg_FilterCfg_Valid(1)
 filter_config_size.FilterCfg_FilterCfg_Active(1)
 
-#信賴率filter
+# 信賴率filter
 filter_config_dist = FilterCfg()
 filter_config_dist.FilterCfg_FilterCfg_Index(0x8)
 filter_config_dist.FilterCfg_FilterCfg_Type(1)
 filter_config_dist.FilterCfg_FilterCfg_Min_Class(3)
 filter_config_dist.FilterCfg_FilterCfg_Max_Class(7)
 filter_config_dist.FilterCfg_FilterCfg_Valid(1)
-filter_config_dist.FilterCfg_FilterCfg_Active(1)
+filter_config_dist.FilterCfg_FilterCfg_Active(0)
 
 config = Radar_Config()
 
 ubyte_3array = c_ubyte * 3
 b = ubyte_3array(0, 0, 0)
-vci_can_obj = VCI_CAN_OBJ(0x200, 0, 0, 1, 0, 0, 8, config.buf, b)  #单次发送
+vci_can_obj = VCI_CAN_OBJ(0x200, 0, 0, 1, 0, 0, 8, config.buf, b)  # 单次发送
 vci_can_obj2 = VCI_CAN_OBJ(0x202, 0, 0, 1, 0, 0, 8, filter_config_size.buf, b)
 vci_can_obj3 = VCI_CAN_OBJ(0x202, 0, 0, 1, 0, 0, 8, filter_config_dist.buf, b)
 ret = canDLL.VCI_Transmit(VCI_USBCAN2, 0, 0, byref(vci_can_obj), 1)
